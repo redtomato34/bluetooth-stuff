a6f0b07f59d1618bbbc7f19afcc78471da03cca1
name <email>
2024-09-13 12:31:15 -0700

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -10,39 +10,35 @@
         And too many stackoverflow links
 */
 #![cfg_attr(
-    all(
-      target_os = "windows",
-      not(debug_assertions),
-    ),
+    all(target_os = "windows", not(debug_assertions),),
     windows_subsystem = "windows"
 )]
-mod util;
-mod render;
 mod bluetooth;
+mod render;
+mod util;
 
 use std::sync::Arc;
 
-use bluetooth::{run_bluetooth_thread, BluetoothInfo};
+use bluetooth::{init_bluetooth_thread, BluetoothInfo};
 use futures::lock::Mutex;
 use render::run_render_thread;
 use util::share_bluetooth_info;
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bluetooth_info = BluetoothInfo {
-        connected_device: Arc::new(Mutex::new(None))
+        connected_device: Arc::new(Mutex::new(None)),
     };
     let display_info = share_bluetooth_info(&bluetooth_info);
     let update_info = share_bluetooth_info(&bluetooth_info);
-    
+
     // run bluetooth thread
-    // todo: spawn new thread for each device
+    // todo: spawn new child thread for each device
+    let bluetooth_thread_handle = init_bluetooth_thread(update_info).await;
     // run render on main thread for tray
-    // if any exit, don't crash the program
-    let bluetooth_thread_handle = run_bluetooth_thread(update_info).await;
     run_render_thread(display_info).await;
-    bluetooth_thread_handle.abort();
+    // if any exit, don't crash the program
     // should never execute this unless user pressed quit in tray menu
+    bluetooth_thread_handle.abort();
     Ok(())
 }
--- src/util.rs
+++ src/util.rs
@@ -7,11 +7,11 @@ use crate::bluetooth::BluetoothInfo;
 const ICON_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "\\icons\\");
 
 pub static WRITE_COMMANDS: [&str; 5] = [
-    "BRSF:23", // m,inimum required for Cowin headphones to work
+    "BRSF:23", // minimum required for Cowin headphones to work
     "+CIND: (\"service\",(0,1)),(\"call\",(0,1))",
     "+CIND: 1,0",
     "+CHLD: 0",
-    "+XAPL=iPhone,6"
+    "+XAPL=iPhone,6",
 ];
 
 pub static READ_COMMANDS: [&str; 6] = [
@@ -20,26 +20,31 @@ pub static READ_COMMANDS: [&str; 6] = [
     "AT+CIND?",
     "AT+CHLD=?",
     "AT+XAPL",
-    "AT+IPHONEACCEV"
+    "AT+IPHONEACCEV",
 ];
 
 pub fn load_icons() -> Option<Vec<Icon>> {
     let mut battery_icons: Vec<Icon> = Vec::new();
     let file_path = Path::new(ICON_PATH);
     let icon_paths = fs::read_dir(file_path).unwrap();
-    
+
     for icon in icon_paths {
         let (_, icon_rgba, icon_width, icon_height) = {
-            let mut img = image::open(icon.as_ref().unwrap().path())
-                .expect("Failed to open icon path");
+            let mut img =
+                image::open(icon.as_ref().unwrap().path()).expect("Failed to open icon path");
             // todo: check for device theme preferences
             img.invert();
             let inverted_img = img.into_rgba8();
             let (width, height) = inverted_img.dimensions();
             let rgba = inverted_img.clone().into_raw();
-            (icon.unwrap().file_name().into_string().unwrap(), rgba, width, height)
+            (
+                icon.unwrap().file_name().into_string().unwrap(),
+                rgba,
+                width,
+                height,
+            )
         };
-        
+
         let create_icon = tray_icon::Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
 
         battery_icons.push(create_icon);
